f66491865493b13cb2894d554b818f3e99435076
Kress50 <kondalovi@gmail.com>
2022-12-21 12:48:49 +0400

    small fixes

--- src/components/GameBoard/Timer.jsx
+++ src/components/GameBoard/Timer.jsx
@@ -1,6 +1,5 @@
 import { Typography } from "@mui/joy";
 import React, { useContext } from "react";
-import { useState } from "react";
 import { useEffect } from "react";
 import GameContext from "../../store/game-context";
 
